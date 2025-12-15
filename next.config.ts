d679fa10c5370cdac1e2383437be36ec1acf51a3
Skyyyisthelimitt <skyyyisthelimitt@gmail.com>
2025-12-15 22:15:14 +0800

    config: Allow external image patterns for exchanges

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,26 @@ const nextConfig = {
 
   // optional but good practice
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "coin-images.coingecko.com",
+      },
+      {
+        protocol: "https",
+        hostname: "upload.wikimedia.org",
+      },
+      {
+        protocol: "https",
+        hostname: "www.bybit.com",
+      },
+      {
+        protocol: "https",
+        hostname: "public.bnbstatic.com",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
